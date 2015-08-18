3706bb69410c9e659f4ad2ad4848543e2e8392a3
Randy Tolentino <makertolentino@gmail.com>
2015-08-18 04:55:35 -0500

    spaces

--- App/Components/Main.js
+++ App/Components/Main.js
@@ -1,31 +1,31 @@
 var React = require('react-native');
 
 var {
-	View,
-	Text,
-	StyleSheet
+  View,
+  Text,
+  StyleSheet
 } = React;
 
 
 class Main extends React.Component{
-	render(){
-		return(
-			<View style={styles.mainContent}>
-				<Text style={styles.mainText}>Testing the Router</Text>
-			</View>
-		)
-	}
+  render(){
+    return(
+      <View style={styles.mainContent}>
+        <Text style={styles.mainText}>Testing the Router</Text>
+      </View>
+    )
+  }
 };
 
 var styles = StyleSheet.create({
-	mainContent: {
-		flex: 1,
-		padding: 30,
-		marginTop: 65,
-		flexDirection: 'column',
-		justifyContent: 'center',
-		backgroundColor: '#48BBEC'
-	}
+  mainContent: {
+    flex: 1,
+    padding: 30,
+    marginTop: 65,
+    flexDirection: 'column',
+    justifyContent: 'center',
+    backgroundColor: '#48BBEC'
+  }
 });
 
 module.exports = Main;
