17f11e9dc04367acedb73f581eed92f01d1eaaa8
Randy Tolentino <makertolentino@gmail.com>
2015-08-18 04:39:24 -0500

    ch 2

---
+++ App/Components/Main.js
@@ -0,0 +1,34 @@
+var React = require('react-native');
+
+var {
+	View,
+	Text,
+	StyleSheet
+} = React;
+
+
+class Main extends React.Component{
+	render(){
+		return(
+			<View style={styles.mainContent}>
+				<Text style={styles.mainText}>Testing the Router</Text>
+			</View>
+		)
+	}
+};
+
+var styles = StyleSheet.create({
+	mainContent: {
+		flex: 1,
+		padding: 30,
+		marginTop: 65,
+		flexDirection: 'column',
+		justifyContent: 'center',
+		backgroundColor: '#48BBEC'
+	},
+	mainText: {
+		color: '#000000'
+	}
+});
+
+module.exports = Main;
---
+++ index.ios.js
@@ -0,0 +1,44 @@
+'use strict';
+
+var React = require('react-native');
+var Main = require('./App/Components/Main');
+
+var {
+  AppRegistry,
+  StyleSheet,
+  Text,
+  NavigatorIOS,
+  View,
+} = React;
+
+class GithubNoteTaker extends React.Component{
+  render() {
+    return (
+      <NavigatorIOS
+      style={styles.container}
+        initialRoute={{
+          title: 'Github Notetaker',
+          component: Main
+        }} />
+    );
+  }
+};
+
+var styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#F5FCFF',
+  }
+});
+
+AppRegistry.registerComponent('GithubNoteTaker', () => GithubNoteTaker);
+
+
+
+
+
+
+
+
